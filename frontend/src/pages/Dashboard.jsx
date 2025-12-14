b911f3ac4531d9dc2077f82377866844d10c3592
Natalia <nananatalia.dudek@gmail.com>
2025-12-14 16:02:35 +0100

    Added changes.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Dashboard() {
+function Dashboard() {
   return (
-    <div className='containner'>Dashboard</div>
+    <div>Dashboard</div>
   )
 }
 
- 
+export default Dashboard
