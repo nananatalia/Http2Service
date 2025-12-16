da0691a732b4ea029dc554920bc1e4990049427c
Natalia <nananatalia.dudek@gmail.com>
2025-12-16 17:05:04 +0100

    Added HomePage text with styling and logo

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './HomePage.css'
+
+function HomePage() {
+  return (
+    <div className='main-text'>
+        <img src="/assets/sound-bar.svg" alt="logo image" className='logo' />
+        <h1>Analizator Widma</h1>
+        <p>Witaj w naszej internetowej aplikacji.</p>
+        <p>Modyfikuj tak jak chcesz - customizacja kolorów i ustawień pozwala dopasować urządzenie do Twojego stylu!</p>
+
+        <button>Zaczynamy</button>
+    </div>
+  )
+}
+
+export default HomePage
