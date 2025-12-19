9f5f1d3e66c1a4acb55fe83733a1a89ac62f1770
Natalia <nananatalia.dudek@gmail.com>
2025-12-19 16:15:25 +0100

    Added handleLogout function

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './Sidebar.css'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+import axios from 'axios';
 
-function Sidebar() {
+function Sidebar({ user, setUser }) {
   const navigate = useNavigate();
   const [active, setActive] = useState("Urządzenia"); // Domyślnie wchodzimy na "Urządzenia"
 
@@ -12,21 +13,32 @@ function Sidebar() {
     navigate(path);
   }
 
+  const handleLogout = async () => {
+    try {
+      await axios.post('/api/auth/logout'); // Wywołanie endpointu wylogowania na backendzie
+      setUser(null); // Ustawienie użytkownika na null w stanie aplikacji
+      navigate('/login');
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
   return (
     <div className='container'>
       <div className='sidebar'>
         <img src='src/assets/user3.png' alt='Profile Picture' className='prof-img' />
         <div className='user-info'>
-          <h2>John Doe</h2>
+          <h2>{user?.name}</h2>
           <hr />
           <h3 id='info' className={active === "Urządzenia" ? "active" : ""} onClick={() => handleNavigation("Urządzenia", "/urządzenia")}>Urządzenia</h3>
           <h3 className={active === "Ustawienia" ? "active" : ""} onClick={() => handleNavigation("Ustawienia", "/ustawienia")}>Ustawienia</h3>
           <h3 className={active === "Historia" ? "active" : ""} onClick={() => handleNavigation("Historia", "/historia")}>Historia</h3>
           <h3 className={active === "Konto" ? "active" : ""} onClick={() => handleNavigation("Konto", "/konto")}>Konto</h3>
-          <button onClick={() => navigate('/')} className='logout-btn'>Wyloguj się</button>
+          <button onClick={handleLogout} className='logout-btn'>Wyloguj się</button>
         </div>
       </div>
-      <div className='main-content'>
+      <div className='outlet'>
         <Outlet />
       </div>
     </div>
